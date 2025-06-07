9b21643d9b4bdec0813024f7c7d3ac81b91e7592
dl2574 <20144415+dl2574@users.noreply.github.com>
2025-06-06 21:42:46 -0700

    Start work on calculate function

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 
 function clearForm() {
-    const form = document.getElementById('mainForm');
+    const form = document.getElementById("mainForm");
     form.reset()
 }
 
 function calculate() {
-    
+    const starting_weight = document.getElementById("weight")
+    const starting_BF = document.getElementById("bodyfat")
+    const starting_muscle = document.getElementById("muscleMass")
+    const starting_bone = document.getElementById("boneMass")
+
+    const goal_weight = document.getElementById("goalWeight")
+    const goal_fat = document.getElementById("goalFat")
+    const goal_muscle = document.getElementById("goalMuscle")
+
+    if (starting_weight.textContent == "" || 
+        starting_BF == "" || 
+        starting_muscle == "" || 
+        starting_bone == "") {
+            return
+    }
+
 }
